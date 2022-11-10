1baa13307a11f489a44677d5354d8f43fa82429a
Szymon1948 <szymonintrys@gmail.com>
2022-11-10 14:03:24 +0100

    Zliczanie_Punktow cz3

--- app/src/main/java/com/learn/zliczanie_punktow2/MainActivity.java
+++ app/src/main/java/com/learn/zliczanie_punktow2/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.learn.zliczanie_punktow2.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
